a245389e52d055512b387aede19a2a6a0b06c995
B CASTILLO <bryancastillo44@outlook.com>
2020-05-11 15:51:23 -0400

    Added images

--- main.js
+++ main.js
@@ -1,43 +1,112 @@
-
-const slides = document.querySelectorAll('.slides');
-
-console.log(slides);
-
-const sliderImages = document.querySelectorAll("slides img");
-
-console.log(sliderImages)
-
-const nextButton = document.querySelector(".slider_button--right");
- 
-const prevButton = document.querySelector(".slider_button--left");
-
-const auto = false;
-
+const slides = document.querySelectorAll('.slide');
+const next = document.querySelector('.slider_button--right');
+const prev = document.querySelector('.slider_button--left');
+const auto = false; // Auto scroll
 const intervalTime = 5000;
+let slideInterval;
+
+const nextSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for next slide
+  if (current.nextElementSibling) {
+    // Add current to next sibling
+    current.nextElementSibling.classList.add('current');
+  } else {
+    // Add current to start
+    slides[0].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+const prevSlide = () => {
+  // Get current class
+  const current = document.querySelector('.current');
+  // Remove current class
+  current.classList.remove('current');
+  // Check for prev slide
+  if (current.previousElementSibling) {
+    // Add current to prev sibling
+    current.previousElementSibling.classList.add('current');
+  } else {
+    // Add current to last
+    slides[slides.length - 1].classList.add('current');
+  }
+  setTimeout(() => current.classList.remove('current'));
+};
+
+// // Button events
+next.addEventListener('click', (e) =>{
+  nextSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+prev.addEventListener('click', (e) =>{
+  prevSlide();
+  if (auto) {
+    clearInterval(slideInterval);
+    slideInterval = setInterval(nextSlide, intervalTime);
+  }
+});
+
+// Auto slide
+if (auto) {
+  // Run next slide at interval time
+  slideInterval = setInterval(nextSlide, intervalTime);
+}
 
 
-
-
-
-
-  // const nasaApiUrl = 'https://api.nasa.gov/planetary/apod?api_key=EpMZfnrZ9IXNOyfGX5UaaAWSOGDgxQwQizkMduPW'
+ const nasaApiUrl = 'https://api.unsplash.com/photos/?client_id=CnMe0JQxSUcrSzMVcKdoW9cU1PvwCcRA54fN7T-Nemk'
   
 
-  // let nasaApiPromise = fetch(nasaApiUrl)
+ let nasaApiPromise = fetch(nasaApiUrl)
   
-  // 	nasaApiPromise.then(unparsedData=> {
-  //   let parseDataPromise = unparsedData.json()
-  //   parseDataPromise.then(parsedData=> {
-  //     console.log("Parsed the data")
-  //     console.log(parsedData)
-  //    })
-  //     console.log("Got the data!")
-  //   })
-  //   console.log("Sent out for data")
+  	nasaApiPromise.then(unparsedData=> {
+    let parseDataPromise = unparsedData.json()
+    parseDataPromise.then(parsedData=> {
+      // console.log("Parsed the data")
+      console.log(parsedData);
+
+	function data (index, value){
+		console.log(value);
+
+		var imageURL = value.urls.regular;
+		console.log(imageURL);
+	}
+
+     });
+      // console.log("Got the data!")
+    });
+    // console.log("Sent out for data")
 
 
-// fetch('https://api.nasa.gov/planetary/apod?api_key=EpMZfnrZ9IXNOyfGX5UaaAWSOGDgxQwQizkMduPW', {
-//     headers: {
-//         'x-api-key': 'EpMZfnrZ9IXNOyfGX5UaaAWSOGDgxQwQizkMduPW'
-//     }
-// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
